eaddc3142ecaa7d4d0a8cf0bcd2ae7fcd0d71a67
Sarar Aseer <saseer@stuy.edu>
2019-01-14 11:33:36 -0500

    Socket Server

--- client.c
+++ client.c
@@ -1,20 +1,21 @@
-#include "pipe_networking.h"
+#include "networking.h"
 
+int main(int argc, char **argv) {
 
-int main() {
+  int server_socket;
+  char buffer[BUFFER_SIZE];
 
-  int to_server;
-  int from_server;
-  char input[BUFFER_SIZE];
+  if (argc == 2)
+    server_socket = client_setup( argv[1]);
+  else
+    server_socket = client_setup( TEST_IP );
 
-  from_server = client_handshake( &to_server );
-  while(1){
-    printf("%s\n", "What is the input");
-    fgets(input, BUFFER_SIZE, stdin);
-    strtok(input, "\n");
-    write(to_server, input, BUFFER_SIZE);
-
-    read(from_server, input, BUFFER_SIZE);
-    printf("Recieved %s\n", input);
+  while (1) {
+    printf("enter data: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    *strchr(buffer, '\n') = 0;
+    write(server_socket, buffer, sizeof(buffer));
+    read(server_socket, buffer, sizeof(buffer));
+    printf("received: [%s]\n", buffer);
   }
 }
