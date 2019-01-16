84dafcf46ed90c78df231544c96deca3ff50cadd
Sarar Aseer <saseer@stuy.edu>
2019-01-16 08:33:53 -0500

    Finally setup networking

--- client.c
+++ client.c
@@ -1,21 +1,36 @@
 #include "networking.h"
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <stdio.h>
+#define SHMSZ 27
 
 int main(int argc, char **argv) {
-
   int server_socket;
   char buffer[BUFFER_SIZE];
-
+  int shmid;
+  key_t key;
+  int *shm, *pids;
+  key = 5678;
+  shmid = shmget(key, SHMSZ, IPC_CREAT | 0666);
+  shm = shmat(shmid, NULL, 0);
+  pids = shm;
+  int oplace=0;;
   if (argc == 2)
     server_socket = client_setup( argv[1]);
   else
     server_socket = client_setup( TEST_IP );
 
+  
+  
   while (1) {
-    printf("enter data: ");
-    fgets(buffer, sizeof(buffer), stdin);
-    *strchr(buffer, '\n') = 0;
-    write(server_socket, buffer, sizeof(buffer));
-    read(server_socket, buffer, sizeof(buffer));
-    printf("received: [%s]\n", buffer);
+    if(pids[1]>oplace){
+      printf("enter data: ");
+      fgets(buffer, sizeof(buffer), stdin);
+      *strchr(buffer, '\n') = 0;
+      write(server_socket, buffer, sizeof(buffer));
+      read(server_socket, buffer, sizeof(buffer));
+      printf("received: [%s]\n", buffer);
+      oplace++;
+    }
   }
 }
--- forking_server.c
+++ forking_server.c
@@ -1,30 +1,99 @@
 #include "networking.h"
-
+#include <sys/ipc.h> 
+#include <sys/shm.h> 
+#include <stdio.h>
 void process(char *s);
 void subserver(int from_client);
+#define SHMSZ 27
 
 int main() {
-
   int listen_socket;
   int f;
   listen_socket = server_setup();
-
+  int shmid;
+  key_t key;
+  int *shm, *pids;
+  key = 5678;
+  shmid = shmget(key, SHMSZ, IPC_CREAT | 0666);
+  shm = shmat(shmid, NULL, 0);
+  pids = shm;
+  pids[1]=0;
+  for(int i=2; i<=5; i++){
+    pids[i]=0;
+  }
   while (1) {
-
     int client_socket = server_connect(listen_socket);
     f = fork();
-    if (f == 0)
+    if (f == 0){
+      int count=0;
+      int spot=0;
+      for(int i=2; i<=5; i++){
+	if(pids[i]!=getpid()){
+	  count++;
+	  if(pids[i]==0){
+	    spot=i;
+	  }
+	}
+      }
+      if(count==4){
+	pids[spot]=getpid();
+      }
+      count=0;
+      for(int i=2; i<=5; i++){
+ 	if(pids[i]!=0){
+	  count++;
+	}
+      }
+      if(count==4){
+	for(int i=2; i<=5; i++){
+	  pids[i]=0;
+	}
+	pids[1]+=1;
+      }
+      printf("%i\n", pids[1]);
       subserver(client_socket);
-    else
+    }
+    else{
       close(client_socket);
+    }
   }
 }
 
 void subserver(int client_socket) {
   char buffer[BUFFER_SIZE];
-
+  int shmid;
+  key_t key;
+  int *shm, *pids;
+  key = 5678;
+  shmid = shmget(key, SHMSZ, IPC_CREAT | 0666);
+  shm = shmat(shmid, NULL, 0);
+  pids = shm;
   while (read(client_socket, buffer, sizeof(buffer))) {
-
+    int count=0;
+    int spot=0;
+    for(int i=2; i<=5; i++){
+      if(pids[i]!=getpid()){
+	count++;
+	if(pids[i]==0){
+	  spot=i;
+	}
+      }
+    }
+    if(count==4){
+      pids[spot]=getpid();
+    }
+    count=0;
+    for(int i=2; i<=5; i++){
+      if(pids[i]!=0){
+	count++;
+      }
+    }
+    if(count==4){
+      for(int i=2; i<=5; i++){
+	pids[i]=0;
+      }
+      pids[1]+=1;
+    }
     printf("[subserver %d] received: [%s]\n", getpid(), buffer);
     process(buffer);
     write(client_socket, buffer, sizeof(buffer));
